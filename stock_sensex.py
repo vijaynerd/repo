85d5a66bfc458af1839799836f14192147f32c15
vijaynerd <vijaynerd@gmail.com>
2025-03-21 22:26:44 +0530

    fixed pandas access but still need to fix hybrid strategy logic

--- stock_sensex.py
+++ stock_sensex.py
@@ -1,47 +1,47 @@
-from sqlite3 import Timestamp
 import pandas as pd
 import datetime
 import numpy as np
 import matplotlib.pyplot as plt
-from pandas.plotting import scatter_matrix
 import yfinance as yf
-import finance_calculator as fc
-
+import finance_calculator as fc  # Ensure this module has get_xirr function
 
 def compound_interest(principal, rate, time):
- 
-    # Calculates compound interest
-    Amount = principal * (pow((1 + rate / 100), time))
-    CI = Amount - principal
-    #print("Compound interest for years ",time," is", CI)
-    return CI
+    """Calculates compound interest."""
+    amount = principal * pow((1 + rate / 100), time)
+    return amount - principal
 
 def hybrid_investment(inv_day):
-    pd.set_option('mode.chained_assignment',None)
+    pd.set_option('mode.chained_assignment', None)
+
+    # Initialize variables
     cashflow_data = []
     cashflow_data_sensex = []
     cashflow_data_hybrid = []
-    total_balance = 0
     sensex_balance = 0
     arb_balance = 0
 
-    start = "2001-01-20"
-    end = '2023-2-08'
+    # Configuration
+    start = "2001-01-25"
+    end = '2023-02-08'
     sensex_xirr = 14
     arbitrage_xirr = 6
     sip_amount = 0
     sip_day = inv_day
-    investments={pd.Timestamp('2008-01-30'):30000000}
-    #investments={}
-    sensex = yf.download('^BSESN',start,end)
+    investments = {pd.Timestamp('2008-01-30'): 30000000}
+
+    # Download Sensex data
+    sensex = yf.download('^BSESN', start=start, end=end)
+    sensex.index = pd.to_datetime(sensex.index)
+    sensex.columns = sensex.columns.droplevel(1)  # Drop 'Ticker' level
+
+    # Clone DataFrames for calculations
     sensex_ideal = sensex.copy()
     sensex_advantage = sensex.copy()
     arbitrage_ideal = sensex.copy()
 
+    # Initialize SIP-related DataFrames with zero 'Open' values
     sensex_sip = sensex.copy()
-    for ind in sensex_sip.index:
-        sensex_sip['Open'][ind] = 0
-
+    sensex_sip['Open'] = 0
     sensex_sip_value = sensex_sip.copy()
     inv_value = sensex_sip.copy()
     arbitrage_sip = sensex_sip.copy()
@@ -50,209 +50,150 @@ def hybrid_investment(inv_day):
     arb_balance_value = sensex_sip.copy()
     total_balance_value = sensex_sip.copy()
 
-    inv_sensex = sensex.copy()
-    inv_arbitrage = sensex.copy()
-
-    len_sensex = len(sensex.index) -1
-
-    print(sensex_ideal.iat[0,0])
-    print(sensex_ideal.iat[len_sensex,0])
-
-    delta = (sensex_ideal.iat[len_sensex,0] - sensex_ideal.iat[0,0]) / len_sensex
+    len_sensex = len(sensex.index) - 1
+    delta = (sensex_ideal.iat[len_sensex, 0] - sensex_ideal.iat[0, 0]) / len_sensex
 
     i = 0
-
     yr_month_old = ""
-    algo_recommendation_buy = 0
-    for ind in sensex_ideal.index:
+
+    for ind in sensex.index:
         algo_sip = 0
         algo_recommendation_buy = 0
         start_ind = int(i / 365)
-        if((start_ind + 1) * 365 > len_sensex):
-            end_ind = len_sensex
-        else:
-            end_ind = (start_ind + 1) * 365
+        end_ind = min((start_ind + 1) * 365, len_sensex)
+
         days = ind - sensex_ideal.index[0]
         yrs = days.days / 365.25
-        yr_month = str(ind.year) + str(ind.month)
-        #sensex_sip['Open'][ind] = 999
-        if i > 0:
-            sensex_ideal['Open'][ind] =  sensex_ideal.iat[0,0] + compound_interest(sensex_ideal.iat[0,0],sensex_xirr,yrs)
-            arbitrage_ideal['Open'][ind] =  arbitrage_ideal.iat[0,0] + compound_interest(arbitrage_ideal.iat[0,0],arbitrage_xirr,yrs)
-        
-        sensex_advantage['Open'][ind] = ((sensex['Open'][ind] - sensex_ideal['Open'][ind] ) / sensex_ideal['Open'][ind]) * 100
-        
-        if investments.get(ind) is not None:
-            print("invest ",ind,investments[ind])
-            nonsip = investments[ind]
-        else:
-            nonsip = 0
+        yr_month = f"{ind.year}{ind.month}"
 
-        arb_balance = arb_balance + nonsip / arbitrage_ideal['Open'][ind]
-        
-        if (yr_month != yr_month_old and i > 0 and ind.day >= sip_day):
+        if i > 0:
+            sensex_ideal.loc[ind, 'Open'] = sensex_ideal.iat[0, 0] + compound_interest(sensex_ideal.iat[0, 0], sensex_xirr, yrs)
+            arbitrage_ideal.loc[ind, 'Open'] = arbitrage_ideal.iat[0, 0] + compound_interest(arbitrage_ideal.iat[0, 0], arbitrage_xirr, yrs)
+
+        sensex_advantage.loc[ind, 'Open'] = ((sensex.loc[ind, 'Open'] - sensex_ideal.loc[ind, 'Open']) / sensex_ideal.loc[ind, 'Open']) * 100
+
+        nonsip = investments.get(ind, 0)
+
+        arb_balance += nonsip / arbitrage_ideal.loc[ind, 'Open']
+
+        if yr_month != yr_month_old and i > 0 and ind.day >= sip_day:
             sip = sip_amount
             yr_month_old = yr_month
-            print("sensex advantage ",ind,sensex_advantage['Open'][ind],"arb_balance",arb_balance," sensex balance",sensex_balance)
-            if sensex_advantage['Open'][ind] < -5:
-                algo_recommendation_swap_sensex = int((0 - sensex_advantage['Open'][ind]) / 5 ) * 5
-                algo_recommendation_swap_sensex = min(10,algo_recommendation_swap_sensex)
-                if(algo_recommendation_swap_sensex > 0 and arb_balance > 10):
-                    print("Swap recommendation ",ind," swap arbitrage to sensex %",algo_recommendation_swap_sensex)
-                    algo_swap_sensex = algo_recommendation_swap_sensex * arb_balance * arbitrage_ideal['Open'][ind] / 100
-                    arb_balance = arb_balance - algo_swap_sensex / arbitrage_ideal['Open'][ind]
-                    sensex_balance = sensex_balance + algo_swap_sensex / sensex_ideal['Open'][ind]
-                    print("sensex advantage ",sensex_advantage['Open'][ind],"arb_balance",arb_balance * arbitrage_ideal['Open'][ind] ," sensex balance",sensex_balance * sensex_ideal['Open'][ind],"Total Bal", arb_balance * arbitrage_ideal['Open'][ind] + sensex_balance * sensex_ideal['Open'][ind])
-                    print("sensex advantage ",sensex_advantage['Open'][ind],"arb_balance",arb_balance," sensex balance",sensex_balance)
+            print("sensex advantage", ind, sensex_advantage.loc[ind, 'Open'], 
+                  "arb_balance", arb_balance, "sensex balance", sensex_balance)
+            
+            print(f"ind: {ind}, type: {type(ind)}")
+            print(f"Index contains ind? {ind in sensex_advantage.index}")
 
-            if sensex_advantage['Open'][ind] > 5:
-                algo_recommendation_swap_arb = int((sensex_advantage['Open'][ind]) / 5 ) * 5
-                algo_recommendation_swap_arb = min(algo_recommendation_swap_arb,30)
-                if(algo_recommendation_swap_arb > 0 and sensex_balance > 10):
-                    print("Swap recommendation ",ind," swap sensex to arbitrage %",algo_recommendation_swap_arb)
-                    algo_swap_arb = algo_recommendation_swap_arb * sensex_balance * sensex_ideal['Open'][ind] / 100
-                    sensex_balance = sensex_balance - algo_swap_arb / sensex_ideal['Open'][ind]
-                    arb_balance = arb_balance + algo_swap_arb / arbitrage_ideal['Open'][ind]
-                    #print("sensex advantage ",sensex_advantage['Open'][ind],"arb_balance",arb_balance," sensex balance",sensex_balance)
-                    print("sensex advantage ",sensex_advantage['Open'][ind],"arb_balance",arb_balance * arbitrage_ideal['Open'][ind] ," sensex balance",sensex_balance * sensex_ideal['Open'][ind],"Total Bal", arb_balance * arbitrage_ideal['Open'][ind] + sensex_balance * sensex_ideal['Open'][ind])
-                    print("sensex advantage ",sensex_advantage['Open'][ind],"arb_balance",arb_balance," sensex balance",sensex_balance)
+            print(f"ind: {ind}, type: {type(ind)}")
+            print(f"sensex_advantage.columns: {sensex_advantage.columns}")
+            print(f"sensex_advantage.index.dtype: {sensex_advantage.index.dtype}")
+            print(f"sensex_advantage.loc[ind]:\n{sensex_advantage.loc[ind]}")
+            print(f"sensex_advantage.loc[ind, 'Open']: {sensex_advantage.loc[ind, 'Open']}")
+            try:
+                val = sensex_advantage.loc[ind, 'Open']
+                if isinstance(val, pd.Series):
+                    if val < -5:
+                        swap_sensex_pct = min(10, int((-sensex_advantage.loc[ind, 'Open']) / 5) * 5)
+                        if swap_sensex_pct > 0 and arb_balance > 10:
+                            print("Swap recommendation", ind, "swap arbitrage to sensex %", swap_sensex_pct)
+                            swap_amount = swap_sensex_pct * arb_balance * arbitrage_ideal.loc[ind, 'Open'] / 100
+                            arb_balance -= swap_amount / arbitrage_ideal.loc[ind, 'Open']
+                            sensex_balance += swap_amount / sensex_ideal.loc[ind, 'Open']
+                            print("Updated balances - sensex:", sensex_balance * sensex_ideal.loc[ind, 'Open'],
+                                "arbitrage:", arb_balance * arbitrage_ideal.loc[ind, 'Open'])
+            except KeyError:
+                print(f"KeyError: {ind} not found in sensex_advantage")
+            except Exception as e:
+                print(f"Unexpected error at index {ind}: {e}")
+
+            if sensex_advantage.at[ind, 'Open'] < -5:
+                swap_sensex_pct = min(10, int((-sensex_advantage.loc[ind, 'Open']) / 5) * 5)
+                if swap_sensex_pct > 0 and arb_balance > 10:
+                    print("Swap recommendation", ind, "swap arbitrage to sensex %", swap_sensex_pct)
+                    swap_amount = swap_sensex_pct * arb_balance * arbitrage_ideal.loc[ind, 'Open'] / 100
+                    arb_balance -= swap_amount / arbitrage_ideal.loc[ind, 'Open']
+                    sensex_balance += swap_amount / sensex_ideal.loc[ind, 'Open']
+                    print("Updated balances - sensex:", sensex_balance * sensex_ideal.loc[ind, 'Open'],
+                          "arbitrage:", arb_balance * arbitrage_ideal.loc[ind, 'Open'])
+
+            if sensex_advantage.at[ind, 'Open'] > 5:
+                swap_arb_pct = min(30, int(sensex_advantage.loc[ind, 'Open'] / 5) * 5)
+                if swap_arb_pct > 0 and sensex_balance > 10:
+                    print("Swap recommendation", ind, "swap sensex to arbitrage %", swap_arb_pct)
+                    swap_amount = swap_arb_pct * sensex_balance * sensex_ideal.loc[ind, 'Open'] / 100
+                    sensex_balance -= swap_amount / sensex_ideal.loc[ind, 'Open']
+                    arb_balance += swap_amount / arbitrage_ideal.loc[ind, 'Open']
+                    print("Updated balances - sensex:", sensex_balance * sensex_ideal.loc[ind, 'Open'],
+                          "arbitrage:", arb_balance * arbitrage_ideal.loc[ind, 'Open'])
         else:
             sip = 0
-        
-        algo_sip = 0    
+
         total_inv = sip + nonsip + algo_sip
 
-            
-        if i > 0: 
-            sensex_sip['Open'][ind] = sensex_sip.iat[i-1,0] + total_inv / sensex['Open'][ind]
-            sensex_sip_value['Open'][ind] = sensex_sip['Open'][ind] * sensex['Open'][ind]
-            arbitrage_sip['Open'][ind] = arbitrage_sip.iat[i-1,0] + total_inv / arbitrage_ideal['Open'][ind]
-            arbitrage_sip_value['Open'][ind] = arbitrage_sip['Open'][ind] * arbitrage_ideal['Open'][ind]
-            inv_value['Open'][ind] = inv_value.iat[i-1,0] + total_inv
-            sensex_balance_value['Open'][ind] = sensex_balance * sensex_ideal['Open'][ind]
-            arb_balance_value['Open'][ind] = arb_balance * arbitrage_ideal['Open'][ind]
-            total_balance_value['Open'][ind] = arb_balance_value['Open'][ind] + sensex_balance_value['Open'][ind]
-            
-        if (total_inv !=0):
-            cashflow_data.append((ind,(0-total_inv)))
-                
-        
-            
-            #sensex_ideal['Open'][ind] =  sensex_ideal.iat[start_ind,0] + delta_s
-        i = i + 1
+        if i > 0:
+            sensex_sip.loc[ind, 'Open'] = sensex_sip.iloc[i - 1, 0] + total_inv / sensex.loc[ind, 'Open']
+            sensex_sip_value.loc[ind, 'Open'] = sensex_sip.loc[ind, 'Open'] * sensex.loc[ind, 'Open']
+            arbitrage_sip.loc[ind, 'Open'] = arbitrage_sip.iloc[i - 1, 0] + total_inv / arbitrage_ideal.loc[ind, 'Open']
+            arbitrage_sip_value.loc[ind, 'Open'] = arbitrage_sip.loc[ind, 'Open'] * arbitrage_ideal.loc[ind, 'Open']
+            inv_value.loc[ind, 'Open'] = inv_value.iloc[i - 1, 0] + total_inv
+            sensex_balance_value.loc[ind, 'Open'] = sensex_balance * sensex_ideal.loc[ind, 'Open']
+            arb_balance_value.loc[ind, 'Open'] = arb_balance * arbitrage_ideal.loc[ind, 'Open']
+            total_balance_value.loc[ind, 'Open'] = sensex_balance_value.loc[ind, 'Open'] + arb_balance_value.loc[ind, 'Open']
 
-    infy = yf.download('INFY.NS',start,end)
-    wipro = yf.download('WIPRO.NS',start,end)
-    #print(arbitrage_sip_value.iloc[-1][0] )
-    cashflow_data_hybrid = []
+        if total_inv != 0:
+            cashflow_data.append((ind, -total_inv))
+
+        i += 1
+
+    # Append final value on 2023-02-22
+    end_date = pd.Timestamp(datetime.date(2023, 2, 22))
+    cashflow_data.append((end_date, int(arbitrage_sip_value['Open'].iloc[-1])))
     cashflow_data_sensex = cashflow_data.copy()
+    cashflow_data_sensex[-1] = (end_date, int(sensex_sip_value['Open'].iloc[-1]))
     cashflow_data_hybrid = cashflow_data.copy()
+    #cashflow_data_hybrid[-1] = (end_date, int(sensex_balance_value['Open'].iloc[-1]) + int(arb_balance_value['Open'].iloc[-1])))
+    cashflow_data_hybrid[-1] = (end_date, int(sensex_balance_value['Open'].iloc[-1]) + int(arb_balance_value['Open'].iloc[-1]))
 
-    cashflow_data.append((pd.Timestamp(datetime.date(2023, 2, 22)),int(arbitrage_sip_value['Open'].iloc[-1])))
-    cashflow_data_sensex.append((pd.Timestamp(datetime.date(2023, 2, 22)),int(sensex_sip_value['Open'].iloc[-1])))
-    cashflow_data_hybrid.append((pd.Timestamp(datetime.date(2023, 2, 22)),int(sensex_balance_value['Open'].iloc[-1]) + int(arb_balance_value['Open'].iloc[-1])))
-
+    # Print cashflows
     print(cashflow_data)
     print(cashflow_data_sensex)
-    arb_xirr = fc.get_xirr(cashflow_data)
-#    print(cashflow_data_hybrid[0])
-#    print(cashflow_data_hybrid[1])
-    sip_sensex_xirr = fc.get_xirr(cashflow_data_sensex)
 
+    # Calculate XIRR
+    arb_xirr = fc.get_xirr(cashflow_data)
+    sip_sensex_xirr = fc.get_xirr(cashflow_data_sensex)
     hybrid_xirr = fc.get_xirr(cashflow_data_hybrid)
 
-    print("XIRR for savings" , arb_xirr)
-    print("XIRR for Sensex" , sip_sensex_xirr)
-    print("XIRR for Hybrid" , hybrid_xirr)
-    
-    #fig, axes = plt.subplots(nrows=2, ncols=1)
+    print("XIRR for Arbitrage Savings:", arb_xirr)
+    print("XIRR for Sensex SIP:", sip_sensex_xirr)
+    print("XIRR for Hybrid Strategy:", hybrid_xirr)
 
-    fig = plt.figure()
-    ax = fig.add_subplot(221)
+    # Plotting
+    fig = plt.figure(figsize=(15, 10))
 
-
-
-    sensex['Open'].plot(label = 'sensex', figsize = (15,7))
-    sensex_ideal['Open'].plot(label = 'sensex_ideal '+str(sensex_xirr))
-    arbitrage_ideal['Open'].plot(label = 'arbitrage '+str(arb_xirr))
-
-    #sensex_sip_value['Open'].plot(label = 'Sensex sip 50000')
-    #inv_value['Open'].plot(label = 'Invested amount')
-    #infy['Open'].plot(label = "Infosys.NS")
-    #wipro['Open'].plot(label = 'Wipro.NS')
-    plt.title('Prices of sensex Analysis')
-    #print(sensex)
+    ax1 = fig.add_subplot(221)
+    sensex['Open'].plot(label='Sensex')
+    sensex_ideal['Open'].plot(label=f'Sensex Ideal ({sensex_xirr}%)')
+    arbitrage_ideal['Open'].plot(label=f'Arbitrage Ideal ({arbitrage_xirr}%)')
+    plt.title('Sensex vs Ideal Growth')
     plt.legend()
-    #plt.show()
 
-    ax = fig.add_subplot(222)
-    #sensex_sip_value['Open'].plot(label = 'Sensex sip ')
-    inv_value['Open'].plot(label = 'Invested amount')
-    sensex_balance_value['Open'].plot(label = 'Sensex balance value amount')
-    arb_balance_value['Open'].plot(label = 'Arbitrage balance value amount')
-    #arbitrage_sip_value['Open'].plot(label = 'Arbitrage sip')
-
-    #infy['Open'].plot(label = "Infosys.NS")
-    #wipro['Open'].plot(label = 'Wipro.NS')
-    plt.title('Sensex vs Arbitrage arb_xirr = '+'%.2f'%arb_xirr+'sensex_xirr = '+'%.2f'%sip_sensex_xirr)
-    #print(sensex)
+    ax2 = fig.add_subplot(222)
+    inv_value['Open'].plot(label='Invested Amount')
+    sensex_balance_value['Open'].plot(label='Sensex Balance Value')
+    arb_balance_value['Open'].plot(label='Arbitrage Balance Value')
+    plt.title(f'Portfolio Value Comparison\nArbitrage XIRR: {arb_xirr:.2f}% | Sensex XIRR: {sip_sensex_xirr:.2f}%')
     plt.legend()
-    #plt.show()
-    #exit()
 
-
-    ax = fig.add_subplot(223)
-    sensex_advantage['Open'].plot(label = 'sensex', figsize = (15,7))
-    #infy['Open'].plot(label = "Infosys.NS")
-    #wipro['Open'].plot(label = 'Wipro.NS')
-    plt.title('Sensex advantage plot')
-    #print(sensex)
+    ax3 = fig.add_subplot(223)
+    sensex_advantage['Open'].plot(label='Sensex Advantage %')
+    plt.title('Sensex Advantage Over Ideal')
     plt.legend()
+
+    plt.tight_layout()
     plt.show()
 
-    return
-    exit(0)
-
-    sensex['Volume'].plot(label = 'sensex', figsize = (15,7))
-    infy['Volume'].plot(label = "Infosys")
-    wipro['Volume'].plot(label = 'Wipro')
-    plt.title('Volume of Stock traded')
-    plt.legend()
-    plt.show()
-
-    #Market Capitalisation
-    sensex['MarktCap'] = sensex['Open'] * sensex['Volume']
-    infy['MarktCap'] = infy['Open'] * infy['Volume']
-    wipro['MarktCap'] = wipro['Open'] * wipro['Volume']
-    sensex['MarktCap'].plot(label = 'sensex', figsize = (15,7))
-    infy['MarktCap'].plot(label = 'Infosys')
-    wipro['MarktCap'].plot(label = 'Wipro')
-    plt.title('Market Cap')
-    plt.legend()
-    plt.show()
-
-
-    sensex['MA50'] = sensex['Open'].rolling(50).mean()
-    sensex['MA200'] = sensex['Open'].rolling(200).mean()
-    sensex['Open'].plot(figsize = (15,7))
-    sensex['MA50'].plot()
-    sensex['MA200'].plot()
-    data = pd.concat([sensex['Open'],infy['Open'],wipro['Open']],axis = 1)
-    data.columns = ['sensexOpen','InfosysOpen','WiproOpen']
-    scatter_matrix(data, figsize = (8,8), hist_kwds= {'bins':250})
-
-
-    #Volatility
-    sensex['returns'] = (sensex['Close']/sensex['Close'].shift(1)) -1
-    infy['returns'] = (infy['Close']/infy['Close'].shift(1))-1
-    wipro['returns'] = (wipro['Close']/wipro['Close'].shift(1)) - 1
-    sensex['returns'].hist(bins = 100, label = 'sensex', alpha = 0.5, figsize = (15,7))
-    infy['returns'].hist(bins = 100, label = 'Infosysy', alpha = 0.5)
-    wipro['returns'].hist(bins = 100, label = 'Wipro', alpha = 0.5)
-    plt.legend()
-    plt.show()
-
-
-for i in range(5,6):
-    print("day of investment",i)
+# Run hybrid investment strategy for day 5
+for i in range(5, 6):
+    print("Day of investment:", i)
     hybrid_investment(i)
